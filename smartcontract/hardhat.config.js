83fa1664710399b4962a020553cdd01b174ca1c3
eejazishaq <eejaz6@gmail.com>
2022-08-03 17:46:28 +0300

    init

---
+++ smartcontract/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+// 0x8b523F573Fa31e9B77a465116Bb2c0eAf2712c79
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
